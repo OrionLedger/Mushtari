9016478d7beb728ff10d13241ef3e277fdba88e9
tamerfakh <tamer.fakho@gmail.com>
2026-01-20 08:49:40 +0200

    fixing model loading error in xg-boost loader

--- serving/loaders/load_models.py
+++ serving/loaders/load_models.py
@@ -1,15 +1,15 @@
 import xgboost as xgb
 from os import environ
-import joblib
 
 MODELS_DIR = environ.get("MODELS_DIR") or "./models/"
 MODEL_NAME = "xgb_model.pkl"
+booster = xgb.Booster()
 _model = None
 
 def load_model():
     global _model
     if _model is None:
-        _model = joblib.load(f"{MODELS_DIR}{MODEL_NAME}")
+        _model = booster.load_model(f"{MODELS_DIR}{MODEL_NAME}")
 
     return _model
 
